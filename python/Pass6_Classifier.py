46f56074086f94019271ae6b6416ac8fc4425fca
jchiang <>
2008-04-12 23:09:53 +0000

    initial import of package to contain FT1 cuts and event class definitions

---
+++ python/Pass6_Classifier.py
@@ -0,0 +1,46 @@
+"""
+@brief Pass 6 event classes.  See
+http://www-glast.stanford.edu/protected/mail/irf/0157.html
+
+These definitions partition the events into three classes.
+"""
+#
+# $Header$
+#
+from EventClassifier import EventClassifier
+
+meritVariables = """
+CTBClassLevel
+CTBCORE
+CTBBestEnergyProb
+""".split()
+
+#
+# Note that the prefilter cuts applied by ROOT (the TCuts par file
+# option), should contain the cuts that are common to all event
+# classes.
+#
+eventClassCuts = ["(CTBClassLevel==1) || ((CTBCORE<0.1) || (CTBBestEnergyProb<0.1))",
+                  "(CTBClassLevel==2) && (CTBCORE>0.1) && (CTBBestEnergyProb>0.1)",
+                  "(CTBClassLevel==3) && (CTBCORE>0.1) && (CTBBestEnergyProb>0.1)"]
+
+eventClassifier = EventClassifier(eventClassCuts)
+
+if __name__ == '__main__':
+    rows = [{'CTBClassLevel' : 1},
+            {'CTBClassLevel' : 2,
+             'CTBCORE' : 0.05,
+             'CTBBestEnergyProb' : 0.05},
+            {'CTBClassLevel' : 3,
+             'CTBCORE' : 0.05,
+             'CTBBestEnergyProb' : 0.11},
+            {'CTBClassLevel' : 2,
+             'CTBCORE' : 0.11,
+             'CTBBestEnergyProb' : 0.11},
+            {'CTBClassLevel' : 3,
+             'CTBCORE' : 0.11,
+             'CTBBestEnergyProb' : 0.11}]
+    classes = (0, 0, 0, 1, 2)
+
+    for row, id in zip(rows, classes):
+        assert(id == eventClassifier(row))
