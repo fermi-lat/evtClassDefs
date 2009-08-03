df915705fc5fcc701eb8a2ede07065fadeb047ae
jchiang <>
2009-08-03 20:15:04 +0000

    use old Pass6_kluge_Classifier code

--- python/Pass6_Reprocessing_Classifier.py
+++ python/Pass6_Reprocessing_Classifier.py
@@ -1,6 +1,8 @@
 """
-@brief Event classes for Pass 6 reprocessing.  This version just copies
-CTBClassLevel to EVENT_CLASS.
+@brief Pass 6 event classes.  See
+http://www-glast.stanford.edu/protected/mail/irf/0157.html
+
+These definitions partition the events into three classes.
 """
 #
 # $Header$
@@ -9,22 +11,38 @@ from EventClassifier import EventClassifier
 
 meritVariables = """
 CTBClassLevel
+CTBCORE
+CTBBestEnergyProb
+Tkr1FirstLayer
 """.split()
 
-class Pass6ReprocessingClassifier(object):
-    def __init__(self):
-        pass
-    def __call__(self, row):
-        return row['CTBClassLevel']
+#
+# Note that the prefilter cuts applied by ROOT (the TCuts par file
+# option), should contain the cuts that are common to all event
+# classes.
+#
+eventClassCuts = ["(CTBClassLevel==0)", 
+                  "(CTBClassLevel==1) || ((CTBCORE<=0.1) || (CTBBestEnergyProb<=0.1))",
+                  "(CTBClassLevel==2) && (CTBCORE>0.1) && (CTBBestEnergyProb>0.1)",
+                  "(CTBClassLevel==3) && (CTBCORE>0.1) && (CTBBestEnergyProb>0.1)"]
 
-eventClassifier = Pass6ReprocessingClassifier()
+eventClassifier = EventClassifier(eventClassCuts)
 
 if __name__ == '__main__':
-    rows = [{'CTBClassLevel' : 0}, 
-            {'CTBClassLevel' : 1}, 
-            {'CTBClassLevel' : 2},
-            {'CTBClassLevel' : 3},
-            {'CTBClassLevel' : 4}]
-    classes = range(5)
+    rows = [{'CTBClassLevel' : 1},
+            {'CTBClassLevel' : 2,
+             'CTBCORE' : 0.05,
+             'CTBBestEnergyProb' : 0.05},
+            {'CTBClassLevel' : 3,
+             'CTBCORE' : 0.05,
+             'CTBBestEnergyProb' : 0.11},
+            {'CTBClassLevel' : 2,
+             'CTBCORE' : 0.11,
+             'CTBBestEnergyProb' : 0.11},
+            {'CTBClassLevel' : 3,
+             'CTBCORE' : 0.11,
+             'CTBBestEnergyProb' : 0.11}]
+    classes = (1, 1, 1, 2, 3)
+
     for row, id in zip(rows, classes):
         assert(id == eventClassifier(row))
